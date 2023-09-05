765fa9ab6fdbbb2145d260e4707c042b13684aa8
marcocentonze <marco.centonze27@gmail.com>
2023-09-05 15:44:15 +0200

    add current time

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,29 @@
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
 Consigli
 Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
-Da quante ore è composto un giorno?
-Da quanti minuti è composta un'ora?
-Da quanti secondi è composto un minuto?
-Da quanti millisecondi è composto un secondo?
+Da quante ore è composto un giorno? 24
+Da quanti minuti è composta un'ora? 60
+Da quanti secondi è composto un minuto? 60
+Da quanti millisecondi è composto un secondo? 1000
 Quanti millisecondi mi separano da domani alle 9:30?
 Esiste un oggetto JS in grado di gestire le date?
 Esistono dei metodi per trasformare una data in millisecondi? */
+
+//variabili con i vari .newDate ecc
+const now = new Date();
+const tomorrow = new Date(now);
+//+1 per arrivare a domani
+tomorrow.setDate(now.getDate() + 1); 
+// Imposta l'orario alle 9:30 di domani mattina
+tomorrow.setHours(9, 30, 0, 0); 
+//ottengo il tempo preciso che voglio
+const wantedTime = tomorrow.getTime(); 
+console.log(wantedTime);
+
+//funzione countdown
+
+//if per dire quando finisce
+
+//operazion di calcolo
+
+//stampa
